a83d8f62963614e813c82b1075171b5d0622e963
PeterZhang <zhangpeter22@gmail.com>
2023-07-24 14:30:49 +0200

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,6 +78,13 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
-      
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
       });
     });
